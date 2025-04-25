a881feaff54a8e6f869d40c6ebc455f8df10cf2b
David-Jorshuam <david.rufino@esmaltec.com.br>
2025-04-25 15:18:08 -0300

    Html_1

--- testes-py.py
+++ testes-py.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import base64
 import os
-
-# Obtenha o diretório atual deste arquivo
-base_dir = os.getcwd()
+from pathlib import Path
 
 # Função para renderizar o conteúdo SVG
 def render_svg(svg_path, width=None):
-    svg_path = os.path.join(base_dir, svg_path)
+    svg_path = Path(svg_path)
+    svg_content = svg_path.read_text(encoding="utf-8")
     with open(svg_path, "r", encoding="utf-8") as file:
-        svg_content = file.read()
+        
         b64 = base64.b64encode(svg_content.encode("utf-8")).decode("utf-8")
         img_tag = f'<img src="data:image/svg+xml;base64,{b64}"'
         if width:
@@ -28,7 +27,7 @@ with col2:
     st.markdown(f"""
         <div style="display: flex; justify-content: center; align-items: center; gap: 20px;">
             <div>{render_svg('assets/logo_unifor.svg', width=150)}</div>
-            <div>{render_svg('assets/Logo_esm.svg', width=150)}</div>
+            <div>{render_svg('assets/logo_esm.svg', width=150)}</div>
             <div>{render_svg('assets/logo_GEQ.svg', width=150)}</div>
         </div>
     """, unsafe_allow_html=True)
